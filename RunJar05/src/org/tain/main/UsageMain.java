3a6dde6bda76ba8e882ef4ce05c7f7902148d3dd
grtlinux <grtlinux@gmail.com>
2018-11-16 04:30:27 +0900

    .

--- RunJar05/src/org/tain/main/UsageMain.java
+++ RunJar05/src/org/tain/main/UsageMain.java
@@ -2,6 +2,8 @@ package org.tain.main;
 
 import java.util.Arrays;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.tain.utils.ClassUtils;
 
 public class UsageMain {
@@ -13,6 +15,48 @@ public class UsageMain {
 	}
 	
 	///////////////////////////////////////////////////////////////////////////
+
+	private static void test01(String[] args) throws Exception {
+
+		if (flag) {
+			String strJson = "{ 'id': 123, 'name': 'Kiea', 'age': 30, 'addr': 'Seoul' }";
+			JSONObject jsonObject = new JSONObject(strJson);
+			String strJsonObject = jsonObject.toString(2);
+			if (flag) System.out.println(">>>>> " + strJsonObject);
+			
+			Long id = jsonObject.has("id") ? jsonObject.getLong("id") : null;
+			String name = jsonObject.has("name") ? jsonObject.getString("name") : null;
+			Integer age = jsonObject.has("age") ? jsonObject.getInt("age") : null;
+			String addr = jsonObject.has("addr") ? jsonObject.getString("addr") : null;
+			String content = jsonObject.has("content") ? jsonObject.getString("content") : null;
+			if (flag) System.out.printf(">>>>> [%s] [%s] [%s] [%s] [%s]%n", id, name, age, addr, content);
+		}
+		
+		if (flag) {
+			JSONObject jsonObject = new JSONObject();
+			
+			jsonObject.put("id", 123);
+			jsonObject.put("name", "Kiea");
+			jsonObject.put("age", 30);
+			jsonObject.put("addr", "Seoul");
+			
+			if (flag) System.out.println(">>>>> " + jsonObject.toString(2));
+		}
+		
+		if (flag) {
+			JSONObject jsonObject = new JSONObject();
+			
+			JSONArray jsonArray = new JSONArray();
+			jsonArray.put("arr-1");
+			jsonArray.put("arr-2");
+			jsonArray.put("arr-3");
+			jsonArray.put("arr-4");
+			
+			jsonObject.put("arr", jsonArray);
+			
+			if (flag) System.out.println(">>>>> " + jsonObject.toString(2));
+		}
+	}
 
 	private static void test02(String[] args) throws Exception {
 		long val = 123456789012L;
@@ -52,6 +96,7 @@ public class UsageMain {
 		if (flag) System.out.println(">>>>> " + ClassUtils.getClassInfo());
 		
 		if (flag) run01(args);
+		if (flag) test01(args);
 		if (flag) test02(args);
 	}
 }
